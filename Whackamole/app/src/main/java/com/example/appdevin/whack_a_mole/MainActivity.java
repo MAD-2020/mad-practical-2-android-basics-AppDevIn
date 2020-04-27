685093f9d4f87e4a45c6bed70c1c7153300b175b
“AppDevIn” <teamprojectlive@gmail.com>
2020-04-27 16:39:45 +0800

    Removing imports and adding non-hardcoded text

--- Whackamole/app/src/main/java/com/example/appdevin/whack_a_mole/MainActivity.java
+++ Whackamole/app/src/main/java/com/example/appdevin/whack_a_mole/MainActivity.java
@@ -7,10 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 
