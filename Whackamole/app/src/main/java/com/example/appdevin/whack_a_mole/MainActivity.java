bf0e0bfb7b02fc7a996726fd4ac1d9e52ac04c48
“AppDevIn” <teamprojectlive@gmail.com>
2020-04-27 09:15:51 +0800

    App created

---
+++ Whackamole/app/src/main/java/com/example/appdevin/whack_a_mole/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdevin.whack_a_mole;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
